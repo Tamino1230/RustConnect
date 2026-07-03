53ee7a37fe05f350a6e94ee2aa8d8afdd57f47ed
Tamino1230 <roidingertamino@gmail.com>
2026-07-03 10:14:29 +0200

    fix: fixed build.rs to windows only

--- src/App/build.rs
+++ src/App/build.rs
@@ -1,9 +1,13 @@
 
+#[cfg(target_os = "windows")]
 use winres;
 
 // building
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("icon.ico");
-    res.compile().unwrap();
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.compile().unwrap();
+    }
 }
