f119fd3a081a66f147f8abaedaf50971ad2a6711
Tamino1230 <roidingertamino@gmail.com>
2026-07-06 14:02:28 +0200

    adjust: adjusted Ubuntu Version to build (musl).

--- src/Build/linuxAppBuild.sh
+++ src/Build/linuxAppBuild.sh
@@ -3,11 +3,12 @@ set -e
 
 cd "$(dirname "$0")/../App"
 
-cargo build --release
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target x86_64-unknown-linux-musl
 
 mkdir -p ../build
 
-cp -f ./target/release/RustConnect ../build/RustConnect
+cp -f ./target/x86_64-unknown-linux-musl/release/RustConnect
 
 echo
 echo "RustConnect is now in ./build"
